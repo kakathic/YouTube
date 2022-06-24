039fc2cf7d8f48d6413260e0ac9a817a238a7da4
Nguyễn Minh Hiếu <kakathic@gmail.com>
2022-06-24 07:11:14 +0700

    Update home.sh

--- home.sh
+++ home.sh
@@ -4,7 +4,7 @@ PHOME="$TOME/Module/YouTube-Cli"
 [ -e $PHOME/lib/res.zip ] && unzip -qo $PHOME/lib/res.zip -d $PHOME/lib
 [ -e $PHOME/lib/prebuilt.zip ] && unzip -qo $PHOME/lib/prebuilt.zip -d $PHOME/lib
 
-if [ -e $PHOME/lib/revanced-cli.jar ];then
+if [ ! -e $PHOME/lib/revanced-cli.jar ];then
 Tv1="$(Xem https://github.com/revanced/revanced-cli/releases | grep '/releases/download' | grep -m1 '.jar' | cut -d \" -f2)"
 Taive "https://github.com$Tv1" $PHOME/lib/revanced-cli.jar
 if [ -e $PHOME/lib/revanced-cli.jar ];then
